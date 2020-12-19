bd0c49ae339a86c3a8da1c2d53bc7ff6d5c8bba5
Vu Nguyen <vunguyen4920@gmail.com>
2020-12-19 21:25:52 +0700

    hello world!

--- src/main/java/com/helloworld/HelloWorldApplication.java
+++ src/main/java/com/helloworld/HelloWorldApplication.java
@@ -6,6 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldApplication {
 
+	/**
+	 * 
+	 * When the JAR file is executed the main() method will be run. The main()
+	 * method calls the static run() method from SpringApplication class, which will
+	 * bootstrap the application, creating the Spring application context. The two
+	 * parameters which are passed to the run() method is the configuration class
+	 * and the command-line arguments.
+	 * 
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
--- src/main/java/com/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -4,12 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Controller: identify this class is the controller
+ * @RequestMapping: handles request for a path. In this example is the root path
+ * 
+ */
 
 @Controller
+// @RestController
 public class HelloWorldController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
     public String helloWorld() {
-        return "Hello World";
+        // return "Hello World Restcontroller";
+        return "hello";
     }
 }
