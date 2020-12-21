3825da479214127a4047be012a76d942a27349a0
Vu Nguyen <vunguyen4920@gmail.com>
2020-12-21 22:58:17 +0700

    update 21/12/2020

--- src/main/java/com/helloworld/controllers/UserController.java
+++ src/main/java/com/helloworld/controllers/UserController.java
@@ -1,16 +1,20 @@
 package com.helloworld.controllers;
 
+import java.net.URI;
 import java.util.List;
 
+import com.exceptions.UserNotFoundException;
 import com.helloworld.beans.User;
 import com.helloworld.dao.UserDAOService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserController {
@@ -31,14 +35,25 @@ public class UserController {
      */
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable Integer id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException("id-" + id);
+        }
+        return user;
     }
 
     /**
      * POST /users/{id}. This method create a user
+     * 
+     * update 21/12/2020: Enhance the POST to return the correct status code. In
+     * this situation, return 201 created instead of 200 OK.
      */
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
         User newUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
